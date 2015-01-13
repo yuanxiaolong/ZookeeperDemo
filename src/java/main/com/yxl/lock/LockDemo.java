8e8eb3d2890d254713d2264e0fb622e58682da92
yuanxiaolong <232351936@qq.com>
2015-01-13 15:44:51 +0800

    zookeeper lock demo

---
+++ src/java/main/com/yxl/lock/LockDemo.java
@@ -0,0 +1,64 @@
+package com.yxl.lock;
+
+import java.io.IOException;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 利用zookeeper实现读写锁的demo
+ * @author xiaolong.yuanxl 
+ */
+public class LockDemo {
+	
+	public static final int corePoolSize = 5;
+	public static final int maximumPoolSize = 10;
+	public static final int keepAliveTime = 3;
+	public static final int maximumLinkedQueueSize = 200000;
+
+	// 固定线程池
+	private static final ThreadPoolExecutor pool = new ThreadPoolExecutor(
+			corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS,
+			new LinkedBlockingQueue<Runnable>(maximumLinkedQueueSize),
+			new ThreadPoolExecutor.AbortPolicy());
+
+	public static void main(String[] args) throws IOException {
+		//先并发写锁
+		for (int i = 0; i < 3; i++) {
+			WriteLockThread thread = new WriteLockThread("Thread-Write-" + (i+1));
+			pool.execute(thread);
+		}
+		//后在写锁基础上,并发读锁
+		for (int i = 0; i < 3; i++) {
+			ReadLockThread thread = new ReadLockThread("Thread-Read-" + (i+1));
+			pool.execute(thread);
+		}
+		
+	}
+	
+	// 应该看到如下的输出
+	/*
+	 * current: /lock/write/write-0000000038
+	 * current: /lock/write/write-0000000039
+	 * current: /lock/write/write-0000000040
+	 * [read theard] register watcher in /lock/write/write-0000000040 success!
+	 * [read theard] register watcher in /lock/write/write-0000000040 success!
+	 * [read theard] register watcher in /lock/write/write-0000000040 success!
+	 * Thread-Write-2 mine: write-0000000040 preview: write-0000000039
+	 * Thread-Write-3 mine: write-0000000039 preview: write-0000000038
+	 * [write thread] register watcher in /lock/write/write-0000000038 success!
+	 * [write thread] register watcher in /lock/write/write-0000000039 success!
+	 * [callback write thread] preview node has been deleted, path: /lock/write/write-0000000038
+	 * path deleted! /lock/write/write-0000000038 do My write Bussiness! Thread-Write-3
+	 * [callback write thread] preview node has been deleted, path: /lock/write/write-0000000039
+	 * path deleted! /lock/write/write-0000000039 do My write Bussiness! Thread-Write-2
+	 * [callback read thread] max write node has been deleted, path: /lock/write/write-0000000040
+	 * [callback read thread] max write node has been deleted, path: /lock/write/write-0000000040
+	 * [callback read thread] max write node has been deleted, path: /lock/write/write-0000000040
+	 * path deleted! /lock/write/write-0000000040 do My read Bussiness! Thread-Read-3
+	 * path deleted! /lock/write/write-0000000040 do My read Bussiness! Thread-Read-2
+	 * path deleted! /lock/write/write-0000000040 do My read Bussiness! Thread-Read-1
+	 */
+	
+	
+}
